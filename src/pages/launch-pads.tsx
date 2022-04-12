c1c330046ff83c09039269798360d971d84a04d1
Oguzhan Olguncu <oguzhan@fireflyon.com>
2022-04-12 12:28:38 +0300

    Add favorite feature to launch pads

--- src/pages/launch-pads.tsx
+++ src/pages/launch-pads.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SimpleGrid } from "@chakra-ui/react";
+import { Button, SimpleGrid, useDisclosure } from "@chakra-ui/react";
 
 import { Error } from "../components/Error";
 import { Breadcrumbs } from "../components/Breadcrumbs";
@@ -8,29 +8,59 @@ import { useSpaceXPaginated } from "../utils/use-space-x";
 import { PAGE_SIZE } from "../constants";
 import { LaunchPad } from "../types/global";
 import { LaunchPadItem } from "../components/LaunchPadItem";
+import { useLocalStorage } from "react-use";
+import FavoriteLaunchPadDrawer from "../components/FavoriteLaunchPadDrawer";
 
 const LaunchPads = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef<HTMLButtonElement>(null);
+
+  const [favorites, setFavorite] = useLocalStorage<LaunchPad[]>(
+    "favoritesLaunchPads",
+    []
+  );
   const { data, error, isValidating, size, setSize } =
     useSpaceXPaginated<LaunchPad>("/launchpads", {
       limit: PAGE_SIZE,
     });
 
+  const handleFavoriteToggle = (launch: LaunchPad) => {
+    const isFavoritePresent = favorites?.find(
+      (favoriteItem) => favoriteItem.id === launch.id
+    );
+    if (isFavoritePresent) {
+      setFavorite([
+        ...(favorites?.filter(
+          (favoriteItem) => favoriteItem.id !== isFavoritePresent.id
+        ) ?? []),
+      ]);
+      return;
+    }
+    setFavorite([...(favorites ?? []), launch]);
+  };
+
+  if (error) return <Error />;
+
   return (
-    <div>
+    <>
       <Breadcrumbs
         items={[
           { label: "Home", to: "/" },
           { label: "Launch Pads", to: "#" },
         ]}
       />
+      <Button m="6" ref={btnRef} onClick={onOpen}>
+        Favorites
+      </Button>
       <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4">
-        {error && <Error />}
-        {data &&
-          data
-            .flat()
-            .map((launchPad) => (
-              <LaunchPadItem key={launchPad.site_id} launchPad={launchPad} />
-            ))}
+        {data?.flat().map((launchPad) => (
+          <LaunchPadItem
+            key={launchPad.site_id}
+            launchPad={launchPad}
+            onFavoriteToggle={handleFavoriteToggle}
+            favorites={favorites}
+          />
+        ))}
       </SimpleGrid>
       <LoadMoreButton
         loadMore={() => setSize(size + 1)}
@@ -38,7 +68,14 @@ const LaunchPads = () => {
         pageSize={PAGE_SIZE}
         isLoadingMore={isValidating}
       />
-    </div>
+
+      <FavoriteLaunchPadDrawer
+        isOpen={isOpen}
+        favorites={favorites}
+        onClose={onClose}
+        onFavoriteToggle={handleFavoriteToggle}
+      />
+    </>
   );
 };
 
